36fdb2545fd98ffdc75d68adc17430b5224a629e
Anthophila1909 <elena.w@hotmail.de>
2024-12-07 09:26:06 +0100

    JS: Added basic form handling and added Typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateFigure(event) {
+  event.preventDefault();
 
+  let generatedElement = document.querySelector("#generated-result");
+
+  new Typewriter(generatedElement, {
+    strings: "placeholder",
+    autoStart: true,
+    delay: 35,
+    cursor: "",
+  });
+}
+
+let generatorFormElement = document.querySelector("#generator-form");
+generatorFormElement.addEventListener("submit", generateFigure);
